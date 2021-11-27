08505c4402d5f4a62ed53a588b4528ebf8405267
Paritosh7 <paritosh7.sahni@gmail.com>
2021-11-27 18:11:28 +0530

    screen - avoiding implementation details

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,18 +2,21 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {screen} from '@testing-library/dom'
-import {render, fireEvent} from '@testing-library/react'
+
+import {render, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
+import userEvent from '@testing-library/user-event'
 
 test('counter increments and decrements when the buttons are clicked', () => {
+  render(<Counter />)
+
   const decrement = screen.getByRole('button', {name: /decrement/i})
   const increment = screen.getByRole('button', {name: /increment/i})
   const message = screen.getByText(/current count: /i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,38 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+function buildLoginForm(overrides) {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  const handleSubmit = jest.fn()
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  const submit = screen.getByRole('button', {name: /submit/i})
+
+  const {username, password} = buildLoginForm({username: 'jk8979kj67'})
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(submit)
+
+  console.log('username: ', username)
+  console.log('password: ', password)
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username,
+    password: password,
+  })
+
+  expect(handleSubmit).toBeCalledTimes(1)
 })
 
 /*
